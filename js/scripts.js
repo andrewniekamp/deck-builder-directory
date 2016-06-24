22ec19ec8a6b6e95c7a4ed932266516b6739be5e
Andrew <andrew.f.niekamp@gmail.com>
2016-06-24 14:30:16 -0700

    add a display area and click function to save and display inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
+//business logic
 function Card(cardName, cardClass) {
   this.name = cardName;
   this.class = cardClass;
 }
+
+//user interface logic
+$(function(){
+  $("form#new-card").submit(function(event) {
+    event.preventDefault();
+    var cardNameInput = $("#new-card-name").val();
+    var cardClassInput = $("#new-card-class").val();
+
+    var newCard = new Card(cardNameInput, cardClassInput);
+
+    $("#deck").append("<li><span class='card'>" + newCard.name + "</span></li>");
+
+    $("#new-card-name").val("");
+    $("#new-card-class").val("");
+
+    $(".card").last().click(function() {
+      $("#show-card").show();
+      $("#show-card h2").text(newCard.name);
+      $(".card-name").text(newCard.name);
+      $(".card-class").text(newCard.class);
+    })
+  })
+})
